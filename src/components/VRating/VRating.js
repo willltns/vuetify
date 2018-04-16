330e6762c23b83dbf3805db22876f57dc9a9f9d6
nekosaur <albert@kaaman.nu>
2018-04-16 23:39:29 +0200

    feature: using v-btns for that MD look

--- src/components/VRating/VRating.js
+++ src/components/VRating/VRating.js
@@ -1,3 +1,4 @@
+import VBtn from '../VBtn'
 import VIcon from '../VIcon'
 import Colorable from '../../mixins/colorable'
 
@@ -45,24 +46,24 @@ export default {
         this.$emit('input', i + 1)
       }
     },
-    genIcon (h, i) {
+    genItem (h, i) {
       const click = this.createClickFn(i)
 
       if (this.$scopedSlots.item) return this.$scopedSlots.item({
         index: i,
         value: this.value,
-        full: this.value > i,
+        filled: this.value > i,
         click,
         hover: this.hover,
         showHover: this.showHover
       })
 
-      let icon
+      let iconName
 
       if (this.showHover && this.hover > 0) {
-        icon = this.hover > i ? this.fullIcon : this.emptyIcon
+        iconName = this.hover > i ? this.fullIcon : this.emptyIcon
       } else {
-        icon = this.value > i ? this.fullIcon : this.emptyIcon
+        iconName = this.value > i ? this.fullIcon : this.emptyIcon
       }
 
       let listeners = {
@@ -80,17 +81,22 @@ export default {
         })
       }
 
-      return h(VIcon, {
+      const icon = h(VIcon, {
         class: this.addTextColorClassChecks({}, this.color),
         on: listeners
+      }, [iconName])
+
+      return h(VBtn, {
+        props: {
+          icon: true,
+          small: true
+        },
+        on: listeners
       }, [icon])
-    },
-    genItems (h) {
-      return createRange(this.length).map(i => this.genIcon(h, i))
     }
   },
   render (h) {
-    const children = this.genItems(h)
+    const children = createRange(this.length).map(i => this.genItem(h, i))
 
     return h('div', {
       class: this.classes
