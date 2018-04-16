78446b3fd0fa420ae521c9162ea646fab02fd9f6
nekosaur <albert@kaaman.nu>
2018-04-16 23:47:34 +0200

    feature: added large prop

--- src/components/VRating/VRating.js
+++ src/components/VRating/VRating.js
@@ -26,6 +26,9 @@ export default {
     },
     showHover: {
       type: Boolean
+    },
+    large: {
+      type: Boolean
     }
   },
   data () {
@@ -82,6 +85,9 @@ export default {
       }
 
       const icon = h(VIcon, {
+        props: {
+          large: this.large
+        },
         class: this.addTextColorClassChecks({}, this.color),
         on: listeners
       }, [iconName])
@@ -89,7 +95,7 @@ export default {
       return h(VBtn, {
         props: {
           icon: true,
-          small: true
+          large: this.large
         },
         on: listeners
       }, [icon])
